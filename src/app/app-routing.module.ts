6322855a585a3b4be12f69c030ea4259c77a4482
daniel dabrowski <agrcom@yahoo.pl>
2018-06-25 20:16:12 +0200

    project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SettlementComponent } from './settlement/settlement.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', children: [] },
+  {path: 'settlement', component: SettlementComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SettlementComponent } from './settlement/settlement.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettlementComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
+    // RouterModule.forRoot(routes, { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
